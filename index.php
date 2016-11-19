e8b4a8be49862cd399b6a0f687b3e21b00950d48
Henry Leon <helg18@gmail.com>
2016-11-19 14:32:08 -0400

    Comenzando con el sitio

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/master.css" media="screen" title="no title">
+    <title>Monjas</title>
+  </head>
+  <body>
+    <header>
+       Guarderia maternal Madre Maria de San José
+    </header>
+    <nav>
+      <ul class="menu">
+        <li class="selected">Inicio</li>
+        <li>Conocenos</li>
+        <li>Servicios</li>
+        <li>Contactanos</li>
+      </ul>
+    </nav>
+    <div class="slider">
+      <ul>
+        <li><a href="#"><img src="img/baner3.png" alt="" /></a></li>
+        <li><a href="#"><img src="img/baner2.png" alt="" /></a></li>
+        <li><a href="#"><img src="img/baner1.png" alt="" /></a></li>
+      </ul>
+    </div>
+    <div class="row">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <a href="#">labore et dolore magna aliqua.</a> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <a href="#">quis nostrud exercitation ullamco laboris</a> nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <div class="row">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </body>
+</html>
